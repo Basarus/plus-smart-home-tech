9e4b5f50ca0c6767309c91b5dfba063acb73cf4b
basarus51 <basarus51@gmail.com>
2026-03-01 04:57:01 +0300

    add

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/CollectorService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/CollectorService.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.telemetry.collector.service;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.telemetry.collector.api.dto.HubEventDto;
 import ru.yandex.practicum.telemetry.collector.api.dto.SensorEventDto;
-import ru.yandex.practicum.telemetry.collector.config.TopicsProperties;
-import ru.yandex.practicum.telemetry.collector.kafka.KafkaProducerService;
+import ru.yandex.practicum.telemetry.collector.kafka.TelemetryProducer;
 import ru.yandex.practicum.telemetry.collector.mapper.AvroEventMapper;
 import ru.yandex.practicum.grpc.telemetry.message.event.HubEventMessagesProto;
 
@@ -18,26 +16,24 @@ import java.time.Instant;
 @RequiredArgsConstructor
 public class CollectorService {
 
-    private final KafkaProducerService producerService;
-    private final TopicsProperties topics;
+    private final TelemetryProducer producer;
     private final AvroEventMapper mapper;
     private final HubEventProtoMapper hubEventProtoMapper;
 
     public void collectSensorEvent(SensorEventDto dto) {
         SensorEventAvro avro = mapper.toAvro(dto);
-        producerService.send(new ProducerRecord<>(topics.getSensors(), avro.getHubId().toString(), avro));
+        producer.sendSensorEvent(avro);
     }
 
     public void collectHubEvent(HubEventDto dto) {
         HubEventMessagesProto.HubEventProto proto = hubEventProtoMapper.toProto(dto);
-        byte[] payload = proto.toByteArray();
 
         Instant ts = Instant.ofEpochSecond(
                 proto.getTimestamp().getSeconds(),
                 proto.getTimestamp().getNanos()
         );
 
-        HubEventAvro avro = mapper.toHubEventAvro(proto.getHubId(), ts, payload);
-        producerService.send(new ProducerRecord<>(topics.getHubs(), proto.getHubId(), avro));
+        HubEventAvro avro = mapper.toHubEventAvro(proto.getHubId(), ts, proto.toByteArray());
+        producer.sendHubEvent(avro);
     }
 }
