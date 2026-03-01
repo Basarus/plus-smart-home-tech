457d7ae9a9a5f3aabf56d4de80e14c295dc2a59a
basarus51 <basarus51@gmail.com>
2026-03-01 06:23:32 +0300

    add

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/grpc/CollectorGrpcService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/grpc/CollectorGrpcService.java
@@ -1,104 +1,159 @@
 package ru.yandex.practicum.telemetry.collector.grpc;
 
 import com.google.protobuf.Empty;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.service.collector.CollectorControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
-import ru.yandex.practicum.grpc.telemetry.message.event.HubEventMessagesProto;
-import ru.yandex.practicum.kafka.telemetry.event.*;
-import ru.yandex.practicum.telemetry.collector.kafka.TelemetryProducer;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.MotionSensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.LightSensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ClimateSensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.SwitchSensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.TemperatureSensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceAddedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceRemovedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioAddedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioRemovedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioConditionProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
 
-import java.nio.ByteBuffer;
+import ru.yandex.practicum.telemetry.collector.api.dto.*;
+import ru.yandex.practicum.telemetry.collector.service.CollectorService;
+
 import java.time.Instant;
+import java.util.List;
 
+@Slf4j
 @GrpcService
+@RequiredArgsConstructor
 public class CollectorGrpcService extends CollectorControllerGrpc.CollectorControllerImplBase {
 
-    private final TelemetryProducer producer;
-
-    public CollectorGrpcService(TelemetryProducer producer) {
-        this.producer = producer;
-    }
+    private final CollectorService collectorService;
 
     @Override
     public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
-        producer.sendSensorEvent(toAvro(request));
-        responseObserver.onNext(Empty.getDefaultInstance());
-        responseObserver.onCompleted();
+        try {
+            SensorEventDto dto = mapSensorEvent(request);
+            collectorService.collectSensorEvent(dto);
+
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (IllegalArgumentException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
+        } catch (Exception e) {
+            log.error("Failed to handle CollectSensorEvent", e);
+            responseObserver.onError(Status.INTERNAL.withDescription("Collector error").asRuntimeException());
+        }
     }
 
     @Override
-    public void collectHubEvent(HubEventMessagesProto.HubEventProto request, StreamObserver<Empty> responseObserver) {
-        producer.sendHubEvent(toAvro(request));
-        responseObserver.onNext(Empty.getDefaultInstance());
-        responseObserver.onCompleted();
+    public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
+        try {
+            HubEventDto dto = mapHubEvent(request);
+            collectorService.collectHubEvent(dto);
+
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (IllegalArgumentException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
+        } catch (Exception e) {
+            log.error("Failed to handle CollectHubEvent", e);
+            responseObserver.onError(Status.INTERNAL.withDescription("Collector error").asRuntimeException());
+        }
     }
 
-    private SensorEventAvro toAvro(SensorEventProto p) {
-        SensorEventAvro avro = new SensorEventAvro();
-        avro.setId(p.getId());
-        avro.setHubId(p.getHubId());
-        avro.setTimestamp(toInstant(p.getTimestamp()));
+    private static SensorEventDto mapSensorEvent(SensorEventProto e) {
+        String sensorId = e.getSensorId();
+        String hubId = e.getHubId();
+        Instant ts = toInstant(e.getTimestamp());
 
-        Object payload = switch (p.getPayloadCase()) {
-            case MOTION_SENSOR -> {
-                MotionSensorAvro a = new MotionSensorAvro();
-                a.setLinkQuality(p.getMotionSensor().getLinkQuality());
-                a.setMotion(p.getMotionSensor().getMotion());
-                a.setVoltage(p.getMotionSensor().getVoltage());
-                yield a;
+        return switch (e.getPayloadCase()) {
+            case MOTION_SENSOR_EVENT -> {
+                MotionSensorEventProto p = e.getMotionSensorEvent();
+                yield new MotionSensorEventDto(sensorId, hubId, ts, p.getMotion(), p.getLinkQuality(), p.getVoltage());
             }
-            case TEMPERATURE_SENSOR -> {
-                TemperatureSensorAvro a = new TemperatureSensorAvro();
-                a.setTemperatureC(p.getTemperatureSensor().getTemperatureC());
-                a.setTemperatureF(p.getTemperatureSensor().getTemperatureF());
-                yield a;
+            case LIGHT_SENSOR_EVENT -> {
+                LightSensorEventProto p = e.getLightSensorEvent();
+                yield new LightSensorEventDto(sensorId, hubId, ts, p.getLuminosity(), p.getLinkQuality(), p.getVoltage());
             }
-            case LIGHT_SENSOR -> {
-                LightSensorAvro a = new LightSensorAvro();
-                a.setLinkQuality(p.getLightSensor().getLinkQuality());
-                a.setLuminosity(p.getLightSensor().getLuminosity());
-                yield a;
+            case CLIMATE_SENSOR_EVENT -> {
+                ClimateSensorEventProto p = e.getClimateSensorEvent();
+                yield new ClimateSensorEventDto(
+                        sensorId, hubId, ts,
+                        p.getTemperatureC(), p.getHumidity(), p.getCo2Level(),
+                        p.getLinkQuality(), p.getVoltage()
+                );
             }
-            case CLIMATE_SENSOR -> {
-                ClimateSensorAvro a = new ClimateSensorAvro();
-                a.setTemperatureC(p.getClimateSensor().getTemperatureC());
-                a.setHumidity(p.getClimateSensor().getHumidity());
-                a.setCo2Level(p.getClimateSensor().getCo2Level());
-                yield a;
+            case SWITCH_SENSOR_EVENT -> {
+                SwitchSensorEventProto p = e.getSwitchSensorEvent();
+                yield new SwitchSensorEventDto(sensorId, hubId, ts, p.getState(), p.getLinkQuality(), p.getVoltage());
             }
-            case SWITCH_SENSOR -> {
-                SwitchSensorAvro a = new SwitchSensorAvro();
-                a.setState(p.getSwitchSensor().getState());
-                yield a;
+            case TEMPERATURE_SENSOR_EVENT -> {
+                TemperatureSensorEventProto p = e.getTemperatureSensorEvent();
+                yield new TemperatureSensorEventDto(sensorId, hubId, ts, p.getTemperatureC(), p.getLinkQuality(), p.getVoltage());
             }
-            case PAYLOAD_NOT_SET -> throw new IllegalArgumentException("Sensor payload is not set");
+            case PAYLOAD_NOT_SET -> throw new IllegalArgumentException("SensorEvent payload is not set");
         };
-
-        avro.setPayload(payload);
-        return avro;
     }
 
-    private HubEventAvro toAvro(HubEventMessagesProto.HubEventProto p) {
-        HubEventAvro avro = new HubEventAvro();
-        avro.setHubId(p.getHubId());
-        avro.setTimestamp(toInstant(p.getTimestamp()));
+    private static HubEventDto mapHubEvent(HubEventProto e) {
+        String hubId = e.getHubId();
+        Instant ts = toInstant(e.getTimestamp());
 
-        byte[] payloadBytes = switch (p.getPayloadCase()) {
-            case DEVICE_ADDED -> p.getDeviceAdded().toByteArray();
-            case DEVICE_REMOVED -> p.getDeviceRemoved().toByteArray();
-            case SCENARIO_ADDED -> p.getScenarioAdded().toByteArray();
-            case SCENARIO_REMOVED -> p.getScenarioRemoved().toByteArray();
-            case DEVICE_ACTION_REQUEST -> p.getDeviceActionRequest().toByteArray();
-            case PAYLOAD_NOT_SET -> throw new IllegalArgumentException("Hub payload is not set");
+        return switch (e.getPayloadCase()) {
+            case DEVICE_ADDED -> {
+                DeviceAddedEventProto p = e.getDeviceAdded();
+                yield new DeviceAddedEventDto(hubId, ts, p.getId(), p.getDeviceType().name());
+            }
+            case DEVICE_REMOVED -> {
+                DeviceRemovedEventProto p = e.getDeviceRemoved();
+                yield new DeviceRemovedEventDto(hubId, ts, p.getId());
+            }
+            case SCENARIO_ADDED -> {
+                ScenarioAddedEventProto p = e.getScenarioAdded();
+
+                List<ScenarioConditionDto> conditions = p.getConditionsList().stream()
+                        .map(CollectorGrpcService::mapCondition)
+                        .toList();
+
+                List<DeviceActionDto> actions = p.getActionsList().stream()
+                        .map(CollectorGrpcService::mapAction)
+                        .toList();
+
+                yield new ScenarioAddedEventDto(hubId, ts, p.getName(), conditions, actions);
+            }
+            case SCENARIO_REMOVED -> {
+                ScenarioRemovedEventProto p = e.getScenarioRemoved();
+                yield new ScenarioRemovedEventDto(hubId, ts, p.getName());
+            }
+
+            case PAYLOAD_NOT_SET -> throw new IllegalArgumentException("HubEvent payload is not set");
+            default -> throw new IllegalArgumentException("Unsupported HubEvent payload: " + e.getPayloadCase());
         };
-
-        avro.setPayload(ByteBuffer.wrap(payloadBytes));
-        return avro;
     }
 
-    private Instant toInstant(com.google.protobuf.Timestamp ts) {
+    private static ScenarioConditionDto mapCondition(ScenarioConditionProto c) {
+        return new ScenarioConditionDto(
+                c.getSensorId(),
+                c.getType().name(),
+                c.getOperation().name(),
+                c.getValue()
+        );
+    }
+
+    private static DeviceActionDto mapAction(DeviceActionProto a) {
+        return new DeviceActionDto(
+                a.getSensorId(),
+                a.getType().name(),
+                a.getValue()
+        );
+    }
+
+    private static Instant toInstant(com.google.protobuf.Timestamp ts) {
         return Instant.ofEpochSecond(ts.getSeconds(), ts.getNanos());
     }
 }
